be5081462358e9a731cc3bcddf03f208018fddf3
ehhaines <haines.e@northeastern.edu>
2023-01-02 04:03:09 -0500

    Change arrow icon to house icon

--- src/about/index.js
+++ src/about/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router";
 import MyParticles from '../particles.js';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 import "./index.css";
 
 const About = () => {
@@ -14,7 +14,9 @@ const About = () => {
         <div className="align-self-center text-center text-secondary eh-blur"> 
           <div className="display-6">
             <p>Hi - thanks for visiting my site! I'm currently earning my masters in computer science at Northeastern University in Boston. Before that, I studied physics at Bowdoin College. Feel free to contact me via Linkedin.</p>
-            <button className="btn text-secondary" onClick={() => nav('/')}><FontAwesomeIcon icon={faArrowLeft} className="display-6"/></button>
+            <div className="text-secondary">
+              <span className="eh-home" title="Go to home" onClick={() => nav('/')}><FontAwesomeIcon icon={faHouse} className="display-6"/></span>
+            </div>
           </div>
         </div>
       </div>
--- src/blog/index.js
+++ src/blog/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
 import { useNavigate } from "react-router";
 import MyParticles from "../particles.js";
 import "./index.css";
@@ -10,7 +10,9 @@ const Blog = () => {
   return(
     <div>
       <MyParticles/>
-      <button className="btn" onClick={() => nav('/')}><FontAwesomeIcon icon={faArrowLeft} className="display-5 text-secondary"/></button>
+      <div className="text-secondary pt-3">
+        <span className="eh-home" title="Go to home" onClick={() => nav('/')}><FontAwesomeIcon icon={faHouse} className="display-5"/></span>
+      </div>
       <div className="col-md-8 offset-md-2 eh-blog">
         <div className="text-center w-100">
           <div className="display-3 w-100 text-secondary">
